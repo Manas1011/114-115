be263ab105e3f925a6752cb92cde683aa28ede5d
Manas1011 <80754667+Manas1011@users.noreply.github.com>
2022-03-16 13:55:13 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function preload(){
+}
+
+function setup(){
+canvas=createCanvas(300,300);
+canvas.centre();
+
+}
+
+function draw(){
+}
+
+function take_snapshot(){
+    save("myFilteredImage.png");
+}
